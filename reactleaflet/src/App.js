da8a6b88054dff898ac5bb22fc61128fe07ded1a
cecil <cecilghimire@gmail.com>
2022-11-15 20:33:40 +0545

    added map and overvier panel

--- reactleaflet/src/App.js
+++ reactleaflet/src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { Map, TileLayer,Marker,Popup, MapContainer} from 'react-leaflet'
+// import {Icon} from 'leaflet'
 function App() {
+  const position = [51.505, -0.09]
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='grid grid-cols-4 gap-4'>
+       <div className='col-span-2'>
+      <MapContainer center={position} zoom={13} scrollWheelZoom={false}>
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"/>
+      <Marker position={position}>
+        <Popup>
+          A pretty CSS3 popup. <br /> Easily customizable.
+        </Popup>
+      </Marker>
+    </MapContainer>
     </div>
+  <div className='bg-cyan-500 text-white col-span-2'> 
+     Overview Panel
+  </div>
+  </div>
   );
 }
 

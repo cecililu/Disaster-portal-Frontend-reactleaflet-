b5f3743b9226af7097a0c36e25bc877bd090e4a5
cecil <cecilghimire@gmail.com>
2022-11-15 20:51:52 +0545

    added nav bar and other css fix

--- reactleaflet/src/App.js
+++ reactleaflet/src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
 import { Map, TileLayer,Marker,Popup, MapContainer} from 'react-leaflet'
+import { Panel } from './Panel';
+import { NavBar } from './NavBar';
 // import {Icon} from 'leaflet'
 function App() {
   const position = [51.505, -0.09]
   return (
-    <div className='grid grid-cols-4 gap-4'>
-       <div className='col-span-2'>
+    <div className='App'>
+      <NavBar/>
+    <div className='grid grid-cols-4 gap-0 h-screen'>
+       <div className='col-span-3'>
       <MapContainer center={position} zoom={13} scrollWheelZoom={false}>
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
@@ -17,8 +21,9 @@ function App() {
       </Marker>
     </MapContainer>
     </div>
-  <div className='bg-cyan-500 text-white col-span-2'> 
-     Overview Panel
+  <div className='bg-cyan-300 text-white col-span-1'>   
+     <Panel/>
+  </div>
   </div>
   </div>
   );

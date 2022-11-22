8b62a13028cca9bf7aebfdeb84ececdc26fedb83
cecil <cecilghimire@gmail.com>
2022-11-22 14:05:23 +0545

    aded edit and delete poly gon functionality

--- reactleaflet/src/MainMap.js
+++ reactleaflet/src/MainMap.js
@@ -23,7 +23,7 @@ export const MainMap = () => {
   
     const layerurl='https://{s}.basemaps.cartocdn.com/rastertiles/voyager_labels_under/{z}/{x}/{y}{r}.png'
     const limeOptions = { color: 'red' }
-    const [polygon, setpolygon] = useState({})
+    const [polygon, setpolygon] = useState([])
     const LalitpurStyle = {
          fillColor:'red',
          fillOpacity:0,
@@ -32,37 +32,43 @@ export const MainMap = () => {
       const position = [27.5602,85.31110094938776]
       const _onCreate=e=>{
       const {layerType,layer}=e
-      if(layerType==='polygon'){
-           const {_leaflet_id}=layer
-           setpolygon(layers=>[
-            ...layers,
-            {    
-              id:_leaflet_id,
-              latlng:layer.getLatLngs()[0]
-            }
-          ])
-      }
-
-
-      }
-      
-
+          if(layerType==='polygon'){
+              const {_leaflet_id}=layer
+              setpolygon(layers=>[
+                ...layers,
+                {    
+                  id:_leaflet_id,
+                  latlng:layer.getLatLngs()[0]
+                }
+              ])
+          }}
+    
       const _onEdit=e=>{
-        console.log(e)
+        const {layer: {_layer}}=e
+        Object.values(_layer).map(({_leaflet_id,editing})=>{
+        setpolygon(layers=>layers.map(l=>l.id===_leaflet_id?{...L,latLngs:{...editing.latlng[0]}}:l))
+       }) 
       }
 
 
       const _onDelete=e=>{
-        console.log(e)
+       const {layers:{_layers}}=e
+       Object.values(_layers).map(_leaflet_id=>{
+        setpolygon(layers=>layers.filter(l=>l.id===_leaflet_id))
+       })
       }
       
 
     return (
       <div>
     <MapContainer center={position} zoom={11} scrollWheelZoom={false}>
-      <FeatureGroup>
+     
+          <GeoJSON  data={district.features} pathOptions={LalitpurStyle} />
+          <TileLayer attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url={layerurl}/>    
+             <FeatureGroup>
         <EditControl 
-        postion='topright'
+          postion='topright'
          onCreated={_onCreate} 
          onEdited={_onEdit} 
          onDeleted={_onDelete}
@@ -74,13 +80,10 @@ export const MainMap = () => {
           marker:false
          }}
          />
-          <GeoJSON  data={district.features} pathOptions={LalitpurStyle} />
-          <TileLayer attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-            url={layerurl}/>    
         <Disaster/>
         </FeatureGroup>
   </MapContainer>
-  {}
+  {JSON.stringify(polygon)}
   </div>
   )
 }
